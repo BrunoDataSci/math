2ccdafe9680971f284b333cb1b2528ba7c9bbf13
BrunoDataSci <136373623+BrunoDataSci@users.noreply.github.com>
2023-10-24 19:30:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ else:
 
 st.write(f"What is {num1} {operator} {num2}?")
 
-user_answer = st.number_input("Enter your answer:", value=100000000000)
+user_answer = st.number_input("Enter your answer:")
 if st.button("Check"):
-    if user_answer != 100000000000:
+    if user_answer != None:
         if user_answer == answer:
             st.success("Correct!")
         else:
