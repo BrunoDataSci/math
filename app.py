d38085d0ba0a8c4fc211855e9d5cdfbbba6c253f
BrunoDataSci <136373623+BrunoDataSci@users.noreply.github.com>
2023-10-24 16:43:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import streamlit as st
 import random
 
-y=0
-while y==0:
+# Function to generate a random math question
+def generate_question():
     num1 = random.randint(1, 100)
     num2 = random.randint(1, 100)
     operator = random.choice(['+', '-', '*', '/'])
-    
-    st.title("Math Quiz App")
-        
-    st.write(f"What is {num1} {operator} {num2}?")
-    
+
     if operator == '+':
         answer = num1 + num2
     elif operator == '-':
@@ -20,24 +16,28 @@ while y==0:
     else:
         # Avoid division by zero
         num2 = random.randint(1, 100)
-        answer = num1 / num2
-        answer=round(answer,2)
-    
-    user_answer = st.number_input("Enter your answer:", key="user_input")
-    
+        answer = round(num1 / num2, 2)
+
+    return num1, num2, operator, answer
+
+# Main Streamlit app
+def main():
+    st.title("Math Quiz App")
+    num1, num2, operator, answer = generate_question()
+    st.write(f"What is {num1} {operator} {num2}?")
+
+    user_answer = st.number_input("Enter your answer:")
+
     if st.button("Check Answer"):
-      
         if user_answer == answer:
             st.success("Correct!")
-            break
         else:
             st.error(f"Wrong. The correct answer is {answer}")
-            st.write(f"The answer was {num1} {operator} {num2} = {answer}")
-            break
-    else:
-        break
-        
+        st.write(f"The answer was {num1} {operator} {num2} = {answer}")
+
     if st.button("Restart"):
-        y=0
-    else:
-        y=1
+        # Restart by regenerating a new question
+        main()
+
+if __name__ == "__main__":
+    main()
