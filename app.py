91ec8a4b2c6e6cd5d2988ee457b757e16f89d27a
BrunoDataSci <136373623+BrunoDataSci@users.noreply.github.com>
2023-10-24 16:29:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,40 @@
 import streamlit as st
 import random
 
-
-num1 = random.randint(1, 100)
-num2 = random.randint(1, 100)
-operator = random.choice(['+', '-', '*', '/'])
-
-st.title("Math Quiz App")
-    
-st.write(f"What is {num1} {operator} {num2}?")
-
-if operator == '+':
-    answer = num1 + num2
-elif operator == '-':
-    answer = num1 - num2
-elif operator == '*':
-    answer = num1 * num2
-else:
-    # Avoid division by zero
+while y==0:
+    num1 = random.randint(1, 100)
     num2 = random.randint(1, 100)
-    answer = num1 / num2
-    answer=round(answer,2)
-
-user_answer = st.number_input("Enter your answer:", key="user_input")
-
-if st.button("Check Answer"):
-  
-    if user_answer == answer:
-        st.success("Correct!")
-        st.empty()
+    operator = random.choice(['+', '-', '*', '/'])
+    
+    st.title("Math Quiz App")
+        
+    st.write(f"What is {num1} {operator} {num2}?")
+    
+    if operator == '+':
+        answer = num1 + num2
+    elif operator == '-':
+        answer = num1 - num2
+    elif operator == '*':
+        answer = num1 * num2
     else:
-        st.error(f"Wrong. The correct answer is {answer}")
-        st.write(f"The answer was {num1} {operator} {num2} = {answer}")
-        st.empty()
+        # Avoid division by zero
+        num2 = random.randint(1, 100)
+        answer = num1 / num2
+        answer=round(answer,2)
     
+    user_answer = st.number_input("Enter your answer:", key="user_input")
     
-
-else:
-    None
+    if st.button("Check Answer"):
+      
+        if user_answer == answer:
+            st.success("Correct!")
+            y==0
+        else:
+            st.error(f"Wrong. The correct answer is {answer}")
+            st.write(f"The answer was {num1} {operator} {num2} = {answer}")
+            y==0
+        
+        
+    
+    else:
+        None
